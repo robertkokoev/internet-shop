280177c9da2b7fc4a388c6c35a8beb4222e95b35
robertkokoev <kokoev.robert@mail.ru>
2021-04-02 20:12:26 +0300

    Наладил сортировку и фильтрацию каталога

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -4,14 +4,20 @@
       <div class="filters-grid">
         Цена
         <div class="row">
-          <v-text-field outlined label="От"></v-text-field>
-          <v-text-field outlined label="До"></v-text-field>
+          <v-text-field v-model="from" outlined label="От"></v-text-field>
+          <v-text-field v-model="to" outlined label="До"></v-text-field>
         </div>
 
         Сортировка
         <div class="row">
-          <v-btn elevation="0">Цена</v-btn>
-          <v-btn elevation="0">Название</v-btn>
+          <v-btn text @click="changeSorting('price')">
+            Цена
+            <v-icon v-if="price !== null">{{ price === 'asc' ? 'expand_less' : 'expand_more' }}</v-icon>
+          </v-btn>
+          <v-btn text @click="changeSorting('name')">
+            Название
+            <v-icon v-if="name !== null">{{ name === 'asc' ? 'expand_less' : 'expand_more' }}</v-icon>
+          </v-btn>
         </div>
       </div>
     </div>
@@ -39,7 +45,11 @@ import firebase from "firebase";
 export default {
   name: 'Catalog',
   data: () => ({
-    goods: []
+    defaultGoods: [],
+    from: 0,
+    to: 99999,
+    price: null,
+    name: null
   }),
   methods: {
     async getGoods() {
@@ -47,13 +57,56 @@ export default {
 
       const value = ref.val();
       const keys = Object.keys(value);
-      this.goods = keys.map(k => ({ ...value[k], id: k }))
-      console.log(this.goods);
+      this.defaultGoods = keys.map(k => ({ ...value[k], id: k }))
+      // console.log(this.goods);
     },
     addToBasket(id) {
       const basketKey = 'basket';
       const currentBasket = JSON.parse(localStorage.getItem(basketKey));
       localStorage.setItem(basketKey, JSON.stringify([...(currentBasket || []), id]))
+    },
+    changeSorting(field) {
+      field !== 'price' ? this.price = null : this.name = null
+
+      if (this[field] === null) {
+        this[field] = 'asc';
+        return;
+      }
+
+      if (this[field] === 'asc') {
+        this[field] = 'desc';
+        return;
+      }
+
+      if (this[field] === 'desc') {
+        this[field] = null;
+      }
+    }
+  },
+  computed: {
+    goods: function () {
+      const filtered = this.defaultGoods.filter(g => g.price > this.from && g.price < this.to )
+
+      filtered.sort((a, b) => {
+        if (this.price === 'asc') {
+          return a.price > b.price ? 1 : -1;
+        }
+
+        if (this.price === 'desc') {
+          return a.price < b.price ? 1 : -1;
+        }
+
+        if (this.name === 'asc') {
+          return a.name > b.name ? 1 : -1;
+        }
+
+        if (this.name === 'desc') {
+          return a.name < b.name ? 1 : -1;
+        }
+      });
+
+      console.log(filtered)
+      return filtered;
     }
   },
   created() {
